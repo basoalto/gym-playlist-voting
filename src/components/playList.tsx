cff9e7dd5a6eaea5569487fb45fbe768302657a7
Lucas <lucasbasoaltocontreras@gmail.com>
2025-05-31 15:29:56 -0400

    add popup

--- src/components/playList.tsx
+++ src/components/playList.tsx
@@ -1,33 +1,37 @@
+import { useEffect, useState } from "react";
 
-import { useEffect, useState } from 'react';
-
-import { getPlayList } from '../services/votesServices';
-
-function PlayList({id}: {id?: string}) {
-
-    const [playList, setPlayList] = useState<any[]>([]);
-    useEffect(() => {
-        const fetchplaylist = async () => {
-            const response = await getPlayList();
-            const playlists = response.filter((playlist) => playlist.gymId === Number(id));
-            setPlayList(playlists);
-        };
-        fetchplaylist()
-    }, [id]);
+import { getPlayList } from "../services/votesServices";
+import PopupVote from "./popupVote";
+function PlayList({ id }: { id?: string }) {
+  const [playList, setPlayList] = useState<any[]>([]);
+  useEffect(() => {
+    const fetchplaylist = async () => {
+      const response = await getPlayList();
+      const playlists = response.filter(
+        (playlist) => playlist.gymId === Number(id)
+      );
+      setPlayList(playlists);
+    };
+    fetchplaylist();
+  }, [id]);
 
   return (
     <div className="playlist">
-        {playList.map((playlist, index) => (
-            <div key={index} className="playlist-item bg-white p-4 rounded-lg shadow hover:shadow-lg transition-shadow duration-200 mb-3">
-                <h2 className="text-xl font-semibold text-amber-600">{playlist.name}</h2>
-                <p className="text-gray-600">Votes: {playlist.votes}</p>
-                <p className="text-gray-600">Gym ID: {playlist.gymId}</p>
-            </div>
-        ))}
+      {playList.map((playlist, index) => (
+        <div
+          key={index}
+          className="playlist-item bg-white p-4 rounded-lg shadow hover:shadow-lg transition-shadow duration-200 mb-3"
+        >
+          <h2 className="text-xl font-semibold text-amber-600">
+            {playlist.name}
+          </h2>
+          <p className="text-gray-600">Votes: {playlist.votes}</p>
+          <p className="text-gray-600">Gym ID: {playlist.gymId}</p>
+          <PopupVote onClose={() => {}} id={playlist.id} />
+        </div>
+      ))}
     </div>
   );
 }
-
-
 
 export default PlayList;
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import AppRouter from "./routes/AppRouter";
 import { BrowserRouter } from "react-router-dom";
+import { VoteProvider } from "./context/contextVote";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    
+      <VoteProvider>
       <BrowserRouter>
         <AppRouter />
       </BrowserRouter>
+   </VoteProvider>
   </StrictMode>
 );
